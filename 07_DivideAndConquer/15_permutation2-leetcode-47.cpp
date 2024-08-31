98a10a50ad73177796fbe80795cd14a2e537918d
AryanTapre <taprearyan7@gmail.com>
2024-08-31 19:19:45 +0530

    D&C/15_permutation2-leetcode-47

---
+++ 07_DivideAndConquer/15_permutation2-leetcode-47.cpp
@@ -0,0 +1,50 @@
+// leetcode 47
+
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<utility> // for make_pair() function
+using namespace std;
+
+void permuteUniqueHelper(vector<int> &nums, vector<vector<int>> &ans, int index) {
+    if(index >= nums.size()) {
+        ans.push_back(nums);
+        return;
+    }
+
+    unordered_map<int, bool> map;
+    for(int i=index; i<nums.size(); i++) {
+        if(map.find(nums.at(i)) != map.end()) {
+            continue;
+        }
+       map[nums[i]] = true;
+
+        swap(nums.at(index), nums.at(i));
+        permuteUniqueHelper(nums,ans, i + 1);
+        swap(nums.at(index), nums.at(i)); // backtracking line..
+    }
+}
+
+vector<vector<int>> permuteUnique(vector<int> &nums) {
+    vector<vector<int>> ans;
+    int index = 0;
+
+    permuteUniqueHelper(nums,ans,index);
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1,1,2};
+    vector<vector<int>> ans =  permuteUnique(nums);
+
+    for(int i=0; ans.size(); i++) {
+        for(int j=0; j<ans[i].size(); j++) {
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+
+return 0;
+}
--- x.cpp
+++ x.cpp
@@ -1,19 +1,11 @@
-#include<unordered_map>
+#include<vector>
 #include<iostream>
 using namespace std;
 
 int main() {
 
-    unordered_map<int, int> map;
-    map[2] = 1;
-    map[3] = 1;
-
-    if(!map[2] ) {
-        cout<<"true";
-    } else {
-        cout<<"false";
-    }
-    cout<<endl;
+    vector<int> ans = {1,2.3};
+    cout<<ans<<endl;
 
 return 0;
 }
