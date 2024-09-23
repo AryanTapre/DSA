325c88699a24fb48ce4ec18b2f8983b6535f8664
AryanTapre <taprearyan7@gmail.com>
2024-09-23 17:43:29 +0530

    linkedlist started

---
+++ 10_linkedlist/single-linkedlist/01_insertAtBeginning.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    private:
+        int data;
+        Node* next;
+
+    public:
+        Node() {
+        }
+};
+
+int main() {
+
+
+return 0;
+}
