4eaed6ee0181af94dcba3409a575bd13c4ac2eb0
AryanTapre <taprearyan7@gmail.com>
2024-09-23 18:36:11 +0530

    singly/insert at end

---
+++ 10_linkedlist/single-linkedlist/02_insertAtEnd.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* next;
+
+        Node() {
+            this->data = 0;
+            this->next = nullptr;
+        }
+
+        Node(int data) {
+            this->data = data;
+            this->next = nullptr;
+        }
+};
+
+void print(Node* HEAD) {    
+    Node* temp = HEAD;
+    while(temp != nullptr) {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void insertAtEnd(Node* &HEAD, Node* &TAIL, int data) {
+    Node* newNode = new Node(data);
+
+    if(HEAD == nullptr) {
+        HEAD = newNode;
+        TAIL = newNode;
+    } else {
+        TAIL->next = newNode;
+        TAIL = newNode;
+    }
+}   
+
+int main() {
+
+    Node* HEAD = nullptr;
+    Node* TAIL = nullptr;
+
+    insertAtEnd(HEAD, TAIL, 10);
+    insertAtEnd(HEAD, TAIL, 20);
+    insertAtEnd(HEAD, TAIL, 30);
+    insertAtEnd(HEAD, TAIL, 40);
+
+    print(HEAD);
+
+    // cout<<endl<<HEAD->data<<endl;
+    // cout<<TAIL->data<<endl;
+
+
+
+return 0;
+}
