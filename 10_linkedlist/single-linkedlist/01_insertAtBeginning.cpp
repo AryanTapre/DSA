c4a10c1b337d3b79778415a8d85aaca7fba1b793
AryanTapre <taprearyan7@gmail.com>
2024-09-23 18:09:13 +0530

    singly/insert at beginning

--- 10_linkedlist/single-linkedlist/01_insertAtBeginning.cpp
+++ 10_linkedlist/single-linkedlist/01_insertAtBeginning.cpp
@@ -2,17 +2,56 @@
 using namespace std;
 
 class Node {
-    private:
+    public: 
         int data;
         Node* next;
-
-    public:
+    
         Node() {
+            this->data = 0;
+            this->next = nullptr;
+        }
+
+        Node(int data) {
+            this->data = data;
+            this->next = nullptr;
         }
 };
 
+void printList(Node* HEAD) {
+    Node* temp = HEAD;
+
+    while(temp != NULL) {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void insertAtBeginning(Node* &HEAD, Node* &TAIL, int data) {
+    Node* newNode = new Node(data);
+
+    if(HEAD == nullptr) {
+        HEAD = newNode;
+        TAIL = newNode;
+        
+    } else {
+        newNode->next = HEAD;
+        HEAD = newNode;
+    }
+}
+
 int main() {
 
+    Node* HEAD = nullptr;
+    Node* TAIL = nullptr;
+
+    insertAtBeginning(HEAD,TAIL,10);
+    insertAtBeginning(HEAD,TAIL,20);
+    insertAtBeginning(HEAD,TAIL,30);
+    insertAtBeginning(HEAD,TAIL,40);
+    insertAtBeginning(HEAD,TAIL,50);
+
+    printList(HEAD);
 
 return 0;
 }
