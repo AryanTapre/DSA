51fa8d6a953bc722951269f2a375d974f9ef010e
AryanTapre <taprearyan7@gmail.com>
2024-10-02 17:34:16 +0530

    sort-0-1-2 without overriding node->data

--- 10_linkedlist/12_sort_0_1_2.cpp
+++ 10_linkedlist/12_sort_0_1_2.cpp
@@ -14,6 +14,21 @@ class Node {
         }
 };
 
+void print(Node* &HEAD) {
+    if(HEAD == nullptr) {
+        cout<<"list is empty"<<endl;
+        return;
+    }
+
+    Node* temp = HEAD;
+    while(temp != nullptr) {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+
 void sort012_coutingApproach(Node* &HEAD) {
     if(HEAD == nullptr) {
         cout<<"list is empty"<<endl;
@@ -45,7 +60,7 @@ void sort012_coutingApproach(Node* &HEAD) {
 
     // overriding the nodes data with repsective counts of 0's, 1's and 2's..
     temp = HEAD;
-  
+
         while(zeroCount--) {
             temp->data = 0;
             temp = temp->next;
@@ -63,27 +78,111 @@ void sort012_coutingApproach(Node* &HEAD) {
     
 }
 
-void print(Node* &HEAD) {
+
+void insertAtEnd(Node* &HEAD, Node* &TAIL, int data) {
+    Node* newNode = new Node(data);
+    if(HEAD == nullptr) {
+        HEAD = newNode;
+        TAIL = newNode;
+    } else {
+        TAIL->next = newNode;
+        TAIL = newNode;
+    }
+}
+
+// without overriding the nodes data.
+Node* sort012(Node* &HEAD) {
     if(HEAD == nullptr) {
         cout<<"list is empty"<<endl;
-        return;
+        return nullptr;
+    }
+    if(HEAD->next == nullptr) {
+        return HEAD;
     }
 
+    // creating pointers..
+    Node* zeroHead = new Node(-1);
+    Node* zeroTail = zeroHead;
+    Node* oneHead = new Node(-1);
+    Node* oneTail = oneHead;
+    Node* twoHead = new Node(-1);
+    Node* twoTail = twoHead;
+
     Node* temp = HEAD;
+    Node* prev = nullptr;
+
+
     while(temp != nullptr) {
-        cout<<temp->data<<" ";
-        temp = temp->next;
+        switch (temp->data) {
+        case 0:
+            cout<<"zero"<<endl;
+            insertAtEnd(zeroHead, zeroTail, temp->data);
+            break;
+        case 1:
+            cout<<"one"<<endl;
+            insertAtEnd(oneHead, oneTail, temp->data);
+            break;
+        case 2:
+            cout<<"two"<<endl;
+            insertAtEnd(twoHead, twoTail, temp->data);
+            break;
+        
+        default:
+            break;
+        }
+
+        temp  = temp->next;
     }
-    cout<<endl;
+
+    if(zeroHead->next != nullptr) {
+        if(oneHead->next != nullptr) {
+            zeroTail->next = oneHead->next;
+            if(twoHead->next != nullptr) {
+                oneTail->next = twoHead->next;
+            }
+        }else if(twoHead->next != nullptr) {
+            zeroTail->next = twoHead->next;
+        }
+        HEAD = zeroHead->next;
+    }else {
+        if(oneHead->next != nullptr) {
+            if(twoHead->next != nullptr) {
+                oneTail->next = twoHead->next;
+            }
+            HEAD = oneHead->next;
+        }else {
+            // only twoHead is available....
+            HEAD = twoHead->next;
+
+        }
+    }
+
+    zeroHead->next = nullptr;
+    oneHead->next = nullptr;
+    twoHead->next = nullptr;
+    
+
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+
+    cout<<"onetail ->"<<oneTail->data<<endl;
+    cout<<"zerotail ->"<<zeroTail->data<<endl;
+    cout<<"twotail ->"<<twoTail->data<<endl;
+    
+
+    return HEAD;
 }
+
+
 
 int main() {
 
-    Node* one = new Node(2);
-    Node* two = new Node(1);
-    Node* three = new Node(0);
+    Node* one = new Node(0);
+    Node* two = new Node(2);
+    Node* three = new Node(2);
     Node* four = new Node(0);
-    Node* five = new Node(0);
+    Node* five = new Node(2);
 
     one->next = two;
     two->next = three;
@@ -91,8 +190,11 @@ int main() {
     four->next = five;
     five->next = nullptr;
 
-    sort012_coutingApproach(one);
-    print(one);
+    // sort012_coutingApproach(one);
+    //print(one);
+    Node* head = sort012(one);
+    
+    print(head);
 
 return 0;
 }
