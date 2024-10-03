4b3d9d9f26dfd92518241680fec8e82114eee2c4
AryanTapre <taprearyan7@gmail.com>
2024-10-03 23:39:44 +0530

    find-kth-node-from-end

---
+++ 10_linkedlist/14_findKNodeFromEnd.cpp
@@ -0,0 +1,22 @@
+// find kth node from end:
+// https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail/problem?isFullScreen=false
+
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+
+        Node(int data) {
+            this->data = data;
+            this->next =  nullptr;
+        }
+
+};
+
+int main() {
+
+return 0;
+}
