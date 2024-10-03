8260792a85adef26ff7b3dbfae8ba39ca90f5d7f
AryanTapre <taprearyan7@gmail.com>
2024-10-03 23:47:27 +0530

    works

--- 10_linkedlist/14_findKNodeFromEnd.cpp
+++ 10_linkedlist/14_findKNodeFromEnd.cpp
@@ -16,13 +16,22 @@ class Node {
 
 };
 
-int getNodeUtility(Node *temp, int position, int data) {
+int getNodeUtility(Node *temp, int position, int &data) {
+    if(temp == nullptr) {
+        return -1;
+    }
+
+    int countPosition = getNodeUtility(temp->next, position, data);
+    ++countPosition;
+        
 
 }
 
 int getNode(Node *&HEAD, int position) {
     Node *temp = HEAD;
     int data = 0;
+
+    return getNodeUtility(temp, position, data);
 }
 
 int main() {
