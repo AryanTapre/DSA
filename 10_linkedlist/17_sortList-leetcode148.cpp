0edc2e444bcb8550aa74080fc53c201380dc932a
AryanTapre <taprearyan7@gmail.com>
2024-10-05 22:18:33 +0530

    17_sort-list-using-merge-sort

---
+++ 10_linkedlist/17_sortList-leetcode148.cpp
@@ -0,0 +1,22 @@
+// merge sort using linked list
+// leetcode: 148
+
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+
+        Node(int _data): data(_data),next(nullptr) {}
+};
+
+Node *sortList(Node *head) {
+    
+}
+
+int main() {
+
+return 0;
+}
