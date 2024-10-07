cd605a8d5fc59793674ac3f9c6e87036ae2a2de1
AryanTapre <taprearyan7@gmail.com>
2024-10-07 06:14:04 +0530

    copy-random-pointer-leetcode-138

---
+++ 10_linkedlist/19_copy-list-with-random-pointer-leetcode138.cpp
@@ -0,0 +1,23 @@
+// leetcode 138
+// copy list with Random Pointer..
+
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node *next;
+        Node *random;
+
+        Node(int _val): val(_val), next(nullptr), random(nullptr) {}
+};
+
+Node *copyRandomList(Node *head) {
+    
+}
+
+int main() {
+
+return 0;
+}
