9d1b5e0fb069af566fc60c069e13b071d5904f93
AryanTapre <taprearyan7@gmail.com>
2024-10-20 23:01:12 +0530

    stacks/find-middle-element-without-loosing-elements

---
+++ 11_stacks/05_find-middle-element-without-losing-stack-element.cpp
@@ -0,0 +1,38 @@
+//TODO: Find the middle element of stack , without compromising any Element!.
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+
+int getMiddleElementHelper(stack<int> &stack, int &totalSize) {
+    if(stack.size() == totalSize+1) return stack.top();
+    
+
+    int temp = stack.top();
+    stack.pop();
+
+    int ele =  getMiddleElementHelper(stack, totalSize);
+    stack.push(temp);
+
+    return ele;
+}
+
+void getMiddleElement(stack<int> &stack) {
+    int totalSize = stack.size() / 2;
+    cout<<getMiddleElementHelper(stack, totalSize)<<endl;
+}
+
+int main() {
+
+    stack<int> stack;
+    stack.push(20);
+    stack.push(10);
+    stack.push(56);
+    stack.push(99);
+    
+
+    getMiddleElement(stack);
+
+return 0;
+}
