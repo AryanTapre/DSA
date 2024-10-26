9d55c96d72905bdb9807c44ac14bdca4e8222448
AryanTapre <taprearyan7@gmail.com>
2024-10-26 18:21:22 +0530

    stacks/alternative-reverse-using-another-stack

--- 11_stacks/13_next-smaller-element.cpp
+++ 11_stacks/13_next-smaller-element.cpp
@@ -5,6 +5,39 @@
 #include<algorithm>
 using namespace std;
 
+
+//FIXME:  ALTERNATE WAY ! reverse using another stack
+
+// vector<int> findNextSmaller(vector<int> &str) {
+//     vector<int> store;
+//     if(str.empty() || str.size() ==1) {
+//         store.push_back(-1);
+//         return store;
+//     }
+
+//     stack<int> st;
+//     stack<int> ans;
+//     st.push(str.back());
+//     ans.push(-1);
+
+//     for(int i=str.size()-2; i>=0; i--) {
+//         if(st.top() < str.at(i)) {
+//             ans.push(st.top());
+//         } else {
+//             st.pop();
+//             st.push(str.at(i));
+//             ans.push(-1);
+//         }
+//     }
+
+//     while(!ans.empty()) {
+//         store.push_back(ans.top());
+//         ans.pop();
+//     }
+//     return store;
+// }
+
+
 vector<int> findNextSmaller(vector<int> &str) {
     vector<int> store;
     if(str.empty() || str.size() ==1) {
@@ -28,11 +61,10 @@ vector<int> findNextSmaller(vector<int> &str) {
 
     reverse(store.begin(), store.end());
     return store;
-
 }
 
 int main() {
-    vector<int> str = {};
+    vector<int> str = {10};
     vector<int> ans = findNextSmaller(str);
 
     for(auto i : ans) {
