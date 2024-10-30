67ad6b607b44611ccea287e4711716f291875184
AryanTapre <taprearyan7@gmail.com>
2024-10-31 00:52:27 +0530

    stacks/udated

--- 11_stacks/14_prev-smaller-element.cpp
+++ 11_stacks/14_prev-smaller-element.cpp
@@ -6,6 +6,7 @@
 #include<stack>
 using namespace std;
 
+
 vector<int> prevSmallerElement(vector<int> &input) {
     vector<int> ans;
     if(input.empty() || input.size() == 1) {
@@ -14,24 +15,47 @@ vector<int> prevSmallerElement(vector<int> &input) {
     }
 
     stack<int> st;
-    ans.push_back(-1);
-    st.push(input.at(0));
+    st.push(-1);
 
-    for(int i=1; i<input.size(); i++) {
-        if(st.top() < input.at(i)) {
-            ans.push_back(st.top());
-        } else {
+    for(int i=0; i<input.size(); i++) {
+        while(st.top() > input[i]) {
             st.pop();
-            st.push(input.at(i));
-            ans.push_back(-1);
         }
+
+        int smallerEle = st.top();
+        ans.push_back(smallerEle);
+
+        st.push(input[i]);
     }
-    
     return ans;
 }
 
+// vector<int> prevSmallerElement(vector<int> &input) {
+//     vector<int> ans;
+//     if(input.empty() || input.size() == 1) {
+//         ans.push_back(-1);
+//         return ans;
+//     }
+
+//     stack<int> st;
+//     ans.push_back(-1);
+//     st.push(input.at(0));
+
+//     for(int i=1; i<input.size(); i++) {
+//         if(st.top() < input.at(i)) {
+//             ans.push_back(st.top());
+//         } else {
+//             st.pop();
+//             st.push(input.at(i));
+//             ans.push_back(-1);
+//         }
+//     }
+    
+//     return ans;
+// }
+
 int main() {
-    vector<int> inp = {15,5,8,7,9};
+    vector<int> inp = {2,1,5,6,2,3};
     vector<int> ans = prevSmallerElement(inp);
 
     for(auto i : ans) cout<<i<<" ";
