46705940be65dd74d47c7533cef8cc7aae2ead0b
AryanTapre <taprearyan7@gmail.com>
2024-11-02 21:40:16 +0530

    works

--- 11_stacks/19_celebrity-problem-gfg-practice.cpp
+++ 11_stacks/19_celebrity-problem-gfg-practice.cpp
@@ -91,7 +91,7 @@ int main() {
     int x = 0;
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
-            m[i][j] = data[x];
+            m[i][j] = data[x]; 
             x++;
         }
     }
