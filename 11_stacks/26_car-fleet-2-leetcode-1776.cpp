84f948301516e811aa5f07b498717f3f92893e40
aryantapre <taprearyan7@gmail.com>
2024-11-20 23:30:00 +0530

    added some files

--- 11_stacks/26_car-fleet-2-leetcode-1776.cpp
+++ 11_stacks/26_car-fleet-2-leetcode-1776.cpp
@@ -1,46 +1,46 @@
-// leetcode: 1776
-// Car Fleet 2
+    // leetcode: 1776
+    // Car Fleet 2
 
-#include<iostream>
-#include<vector>
-#include<stack>
-using namespace std;
+    #include<iostream>
+    #include<vector>
+    #include<stack>
+    using namespace std; 
+    
+    vector<double> getCollisionTimes(vector<vector<int>> &cars) {
+        vector<double> ans(cars.size(),-1);
+        stack<int> st;
+        st.push(cars.size()-1);
 
-vector<double> getCollisionTimes(vector<vector<int>> &cars) {
-    vector<double> ans(cars.size(),-1);
-    stack<int> st;
-    st.push(cars.size()-1);
-
-    for(int i=cars.size()-1; i>=0; --i) {
-        while(!st.empty() && cars[i][1] <= cars[st.top()][1]) {
-            st.pop();
-        }
-        
-        if(!st.empty()) {
-            while(!st.empty()) {
-                double collisionTime = (cars[st.top()][0] - cars[i][0]) / double(cars[i][1] - cars[st.top()][1]);
-                if(ans[st.top()] == -1 || collisionTime < ans[st.top()]) {
-                    ans[i] = collisionTime;
-                    break;
-                }
+        for(int i=cars.size()-1; i>=0; --i) {
+            while(!st.empty() && cars[i][1] <= cars[st.top()][1]) {
                 st.pop();
             }
-            // google.com
+            
+            if(!st.empty()) {
+                while(!st.empty()) {
+                    double collisionTime = (cars[st.top()][0] - cars[i][0]) / double(cars[i][1] - cars[st.top()][1]);
+                    if(ans[st.top()] == -1 || collisionTime < ans[st.top()]) {
+                        ans[i] = collisionTime;
+                        break;
+                    }
+                    st.pop();
+                }
+                // google.com
+            }
+
+            st.push(i);
         }
-
-        st.push(i);
+        return ans;
     }
-    return ans;
-}
 
-int main() {
+    int main() {
 
-    vector<vector<int>> cars = {{3,4},{5,4},{6,3},{9,1}};
-    vector<double> times = getCollisionTimes(cars);
-    for(auto i : times) cout<<i <<" ";
-    cout<<endl;
+        vector<vector<int>> cars = {{3,4},{5,4},{6,3},{9,1}};
+        vector<double> times = getCollisionTimes(cars);
+        for(auto i : times) cout<<i <<" ";
+        cout<<endl;
 
-return 0;
-}
+    return 0;
+    }
 
 
---
+++ 11_stacks/h.js
@@ -0,0 +1,12 @@
+{
+    "files.autoSave": "afterDelay",
+    "editor.mouseWheelZoom": true,
+    "terminal.integrated.fontSize": 11,
+    "terminal.integrated.fontWeight": "bold",
+    "editor.fontSize": 10.5,
+    "terminal.integrated.fontFamily": "monospace",
+    "git.openRepositoryInParentFolders": "never",
+    "editor.fontFamily": "JetBrains Mono,'Droid Sans Mono', 'monospace', monospace",
+    "editor.fontLigatures": true
+
+}
